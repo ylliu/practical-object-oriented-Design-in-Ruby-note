efd57e4c440552ee4e40d7f5fe40b394cc7db102
ylliu <liuming1908@gmail.com>
2022-08-12 17:34:57 +0800

    extract a wheel class

--- lib/gear.rb
+++ lib/gear.rb
@@ -1,11 +1,12 @@
-class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+require_relative '../lib/wheel'
 
-  def initialize(chainring, cog, rim, tire)
+class Gear
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring, cog, wheel)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -13,9 +14,10 @@ class Gear
   end
 
   def gear_inches
-    ratio * (rim + tire * 2)
+    ratio * wheel.diameter
   end
+
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
+
---
+++ lib/wheel.rb
@@ -0,0 +1,18 @@
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
