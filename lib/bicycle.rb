84218608f3a002cee1c02d797809fa11232661fe
ylliu <liuming1908@gmail.com>
2022-08-24 10:20:26 +0800

    Embedding Multiple Types

--- lib/bicycle.rb
+++ lib/bicycle.rb
@@ -1,22 +1,36 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color,
+              :front_shock, :rear_shock
 
   def initialize(args)
+    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
   def spares
-    { chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      { chain: '10-speed',
+        tire_size: '23',
+        tape_color: tape_color
+      }
+    else
+      {
+        chain: '10-speed',
+        tire_size: '2.1',
+        rear_shock: rear_shock
+      }
+    end
   end
 end
 
 bike = Bicycle.new(
-  size:'M',
-  tape_color:'red')
+  style: :mountain,
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox')
 
 puts bike.size
 puts bike.spares
