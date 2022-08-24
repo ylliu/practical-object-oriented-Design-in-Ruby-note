0d6f84fd72bf87afabd398a37e2e41b230821ca2
ylliu <liuming1908@gmail.com>
2022-08-24 10:40:57 +0800

    Misapplying Inheritance

--- lib/bicycle.rb
+++ lib/bicycle.rb
@@ -1,36 +1,38 @@
 class Bicycle
-  attr_reader :style, :size, :tape_color,
-              :front_shock, :rear_shock
+  attr_reader :size, :tape_color
 
   def initialize(args)
-    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
   end
 
   def spares
-    if style == :road
-      { chain: '10-speed',
-        tire_size: '23',
-        tape_color: tape_color
-      }
-    else
-      {
-        chain: '10-speed',
-        tire_size: '2.1',
-        rear_shock: rear_shock
-      }
-    end
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
   end
 end
 
-bike = Bicycle.new(
-  style: :mountain,
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new(
   size: 'S',
   front_shock: 'Manitou',
   rear_shock: 'Fox')
 
-puts bike.size
-puts bike.spares
+puts mountain_bike.size
+puts mountain_bike.spares
