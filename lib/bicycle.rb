282086068abbe35f3e832de39aee40bb932e0bab
ylliu <liuming1908@gmail.com>
2022-08-24 11:10:49 +0800

    Using the Template Method Pattern

--- lib/bicycle.rb
+++ lib/bicycle.rb
@@ -1,9 +1,27 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :size, :chain, :tire_size
+
+  def initialize(args = {})
+    @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+  end
+
+  def default_chain
+    '10-speed'
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
 
   def initialize(args)
-    @size = args[:size]
     @tape_color = args[:tape_color]
+    super(args)
+  end
+
+  def default_tire_size
+    '23'
   end
 
   def spares
@@ -24,15 +42,27 @@ class MountainBike < Bicycle
     super(args)
   end
 
+  def default_tire_size
+    '2.1'
+  end
+
   def spares
     super.merge(rear_shock: rear_shock)
   end
 end
 
-mountain_bike = MountainBike.new(
-  size: 'S',
-  front_shock: 'Manitou',
-  rear_shock: 'Fox')
+road_bike = RoadBike.new(
+  size: 'M',
+  tape_color: 'red'
+)
 
-puts mountain_bike.size
-puts mountain_bike.spares
+puts road_bike.size
+puts road_bike.tire_size
+#
+# mountain_bike = MountainBike.new(
+#   size: 'S',
+#   front_shock: 'Manitou',
+#   rear_shock: 'Fox')
+
+# puts mountain_bike.size
+# puts mountain_bike.spares
