1af2be5610c5b03dc69065c8254390ecb91ee485
ylliu <liuming1908@gmail.com>
2022-09-07 14:46:37 +0800

    Decoupling Subclasses Using Hook Messages

--- lib/bicycle.rb
+++ lib/bicycle.rb
@@ -5,6 +5,11 @@ class Bicycle
     @size = args[:size]
     @chain = args[:chain] || default_chain
     @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args)
+  end
+
+  def post_initialize(args)
+    nil
   end
 
   def default_tire_size
@@ -14,50 +19,68 @@ class Bicycle
   def default_chain
     '10-speed'
   end
+
+  def spares
+    { tire_size: tire_size,
+      chain: chain
+    }.merge(local_spares)
+  end
+
+  def local_spares
+    {}
+  end
 end
 
 class RoadBike < Bicycle
   attr_reader :tape_color
 
-  def initialize(args)
+  def post_initialize(args)
     @tape_color = args[:tape_color]
-    super(args)
   end
 
   def default_tire_size
     '23'
   end
 
-  def spares
-    {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+  def local_spares
+    { tape_color: tape_color }
   end
 end
 
 class MountainBike < Bicycle
   attr_reader :front_shock, :rear_shock
 
-  def initialize(args)
+  def post_initialize(args)
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
-    super(args)
   end
 
   def default_tire_size
     '2.1'
   end
 
-  def spares
-    super.merge(rear_shock: rear_shock)
+  def local_spares
+    { rear_shock: rear_shock }
   end
 end
 
 class RecumbentBike < Bicycle
+  attr_reader :flag
+
+  def post_initialize(args)
+    @flag = args[:flag]
+  end
+
+  def local_spares
+    { flag: flag }
+  end
+
   def default_chain
     '9-speed'
+  end
+
+  def default_tire_size
+    '28'
   end
 end
 
@@ -79,4 +102,5 @@ puts mountain_bike.tire_size
 
 # puts mountain_bike.spares
 
-bent = RecumbentBike.new
+bent = RecumbentBike.new(flag: 'tall and orange')
+puts bent.spares
